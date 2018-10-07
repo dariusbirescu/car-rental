2d61d319224f5d49c376fd97a2384a81b79c1be5
dariusbirescu <darius.birescu@gmail.com>
2018-10-07 21:11:17 +0300

    Implemented login view for logging of the user
    Implemented login view with components like email/user text field, password field, background image, so on. Added a small check if the user/pass is admin/admin and if so, the user gets forwarded to a new layout.  If the credentials are wrong, the user receives a message with "wrong credentials".

---
+++ app/src/main/java/com/example/darius/carrental/Welcome.java
@@ -0,0 +1,17 @@
+package com.example.darius.carrental;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+/**
+ * Created by Darius on 10/7/2018.
+ */
+
+public class Welcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
